4b6e0b39143ad8cc6e95900564485ce126850435
Tim Yang <tim.yang@4alpha-capital.com>
2023-10-02 08:10:51 +0000

    feat: logging update
    1. add log config in OmniUniBotConfig
    2. make server log to file and split by day

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,4 +1,5 @@
 import pytest
+from pathlib import Path
 from omniunibot.common.data_type import OmniUniBotChannelConfig, OmniUniBotConfig
 
 
@@ -17,6 +18,44 @@ def test_channel_config_2():
 
 def test_config_1():
     cfg_dict = {
+        "server": {
+            "bind": "tcp://*:58655",
+            "interval": 0.5,
+        },
+        "client": {
+            "bind": "tcp://localhost:58655",
+        },
+        "log": {
+            "level": "DEBUG",
+            "dir": "/home/timyang/logs/omniunibot",
+        },
+        "channel_groups": {
+            "test_channels": [
+                {
+                    "platform": "Slack",
+                    "webhook": "https://hooks.slack.com/services/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",
+                },
+                {
+                    "platform": "Lark",
+                    "webhook": "https://open.feishu.cn/open-apis/bot/v2/hook/1a166e72-9dae-4989-a9eb-3ae4f0fb51b7",
+                    "secret": "eJ3EE0Ghn5Dp0z11O9k9Sg",
+                },
+                {
+                    "platform": "DingTalk",
+                    "webhook": "https://oapi.dingtalk.com/robot/send?access_token=9a045513ee68234eed36308d579901edd5a83e9413d9619233280a1ad6f6b5e9",
+                    "secret": "SECbba89f6262b8b2823bb05013df307dc850258e695cd202b5519ad011234fe72e",
+                },
+            ]
+        },
+    }
+    cfg = OmniUniBotConfig.from_dict(cfg_dict)
+    assert cfg == OmniUniBotConfig.from_dict(cfg_dict)
+    assert cfg.to_dict() == cfg_dict
+
+
+def test_config_2():
+    # test config without log
+    cfg_dict_abbr = {
         "server": {
             "bind": "tcp://*:58655",
             "interval": 0.5,
@@ -43,6 +82,37 @@ def test_config_1():
             ]
         },
     }
-    cfg = OmniUniBotConfig.from_dict(cfg_dict)
-    assert cfg == OmniUniBotConfig.from_dict(cfg_dict)
-    assert cfg.to_dict() == cfg_dict
+    cfg_dict_full = {
+        "server": {
+            "bind": "tcp://*:58655",
+            "interval": 0.5,
+        },
+        "client": {
+            "bind": "tcp://localhost:58655",
+        },
+        "log": {
+            "level": "DEBUG",
+            "dir": str(Path.home() / "logs" / "omniunibot"),
+        },
+        "channel_groups": {
+            "test_channels": [
+                {
+                    "platform": "Slack",
+                    "webhook": "https://hooks.slack.com/services/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",
+                },
+                {
+                    "platform": "Lark",
+                    "webhook": "https://open.feishu.cn/open-apis/bot/v2/hook/1a166e72-9dae-4989-a9eb-3ae4f0fb51b7",
+                    "secret": "eJ3EE0Ghn5Dp0z11O9k9Sg",
+                },
+                {
+                    "platform": "DingTalk",
+                    "webhook": "https://oapi.dingtalk.com/robot/send?access_token=9a045513ee68234eed36308d579901edd5a83e9413d9619233280a1ad6f6b5e9",
+                    "secret": "SECbba89f6262b8b2823bb05013df307dc850258e695cd202b5519ad011234fe72e",
+                },
+            ]
+        },
+    }
+    cfg = OmniUniBotConfig.from_dict(cfg_dict_abbr)
+    assert cfg == OmniUniBotConfig.from_dict(cfg_dict_full)
+    assert cfg.to_dict() == cfg_dict_full
