d725960201de3b148d3495b91de0d74cf12f501c
yttty <yttty@noreply.com>
2024-03-26 14:57:15 +0800

    chore: update dependencies

--- omniunibot/__init__.py
+++ omniunibot/__init__.py
@@ -1,5 +1,3 @@
 from .server.server import OmniUniBotServer
 from .client.client import OmniUniBotClient
 from .common.data_type import OmniUniBotConfig, Msg, MsgType
-
-from .__version__ import __version__
